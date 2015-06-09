f33034694d249e17bc1a8c1f641b522887f7e966
mlindroos <mira.lindroos@aalto.fi>
2015-06-10 00:05:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,18 +31,20 @@ $(document).ready(function() {
 });
 function change() {
     console.log("orkif")
-    if(index == 0) {
-        $("header").css("background-image", "url(https://cloud.githubusercontent.com/assets/10785456/8024126/323d26cc-0d31-11e5-9f12-d95937216843.jpg)");
-    }
-    else if(index == 1) {
-        $("header").css("background-image", "url(https://cloud.githubusercontent.com/assets/10785456/8069202/68778b5c-0f01-11e5-8505-0319064c82ab.jpg)");
-    }
-    else {
-    $("header").css("background-image", "url(https://cloud.githubusercontent.com/assets/10785456/8069406/f554b36e-0f02-11e5-8da4-ba47f68dff15.jpg)");
-    index = -1;
-    }
-    index = index + 1;
+    $("header").fadeTo("slow", 0.3, function() {
+        if(index == 0) {
+            $("header").css("background-image", "url(https://cloud.githubusercontent.com/assets/10785456/8024126/323d26cc-0d31-11e5-9f12-d95937216843.jpg)");
+        }
+        else if(index == 1) {
+            $("header").css("background-image", "url(https://cloud.githubusercontent.com/assets/10785456/8069202/68778b5c-0f01-11e5-8505-0319064c82ab.jpg)");
+        }
+        else {
+        $("header").css("background-image", "url(https://cloud.githubusercontent.com/assets/10785456/8069406/f554b36e-0f02-11e5-8da4-ba47f68dff15.jpg)");
+        index = -1;
+        }
+        index = index + 1;
 }
+});
 $( "#navigation" ).on("mouseover", function() {
     console.log("HEI");
      $(this).css("color", "red");
