767358b2422fb4d4cd8aa3bd6a85bf91b83b733d
mlindroos <mira.lindroos@aalto.fi>
2015-06-10 00:09:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 });
 function change() {
     console.log("orkif")
-    $("header").fadeTo("slow", 0.8, function() {
+    $("header").fadeTo("slow", 1.0, function() {
         if(index == 0) {
             $("header").css("background-image", "url(https://cloud.githubusercontent.com/assets/10785456/8024126/323d26cc-0d31-11e5-9f12-d95937216843.jpg)");
         }
